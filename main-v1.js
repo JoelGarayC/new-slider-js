33ab1b7fb78f07d728bd3546b829c100b5e808c5
Joel Garay <joelgaray2020@gmail.com>
2023-12-10 23:09:14 -0500

    add: first init

--- main-v1.js
+++ main-v1.js
@@ -4,6 +4,18 @@ const ITEMS = 20
 const ITEMS_ROW = 2
 const SPEED_ITEMS_ROW = [0.4, 0.7]
 const DIRECTION_ROW = ['left', 'left']
+const DESING = {
+  mobile: {
+    widthItem: '178px',
+    heightItem: '100px',
+    gapSlider: '0.625rem'
+  },
+  desktop: {
+    widthItem: '380px',
+    heightItem: '214px',
+    gapSlider: '1.25rem'
+  }
+}
 
 function handleTranslateSlider(sliderWrapper, speed, direction, middleIndex) {
   const slider = sliderWrapper.children[0]
@@ -202,10 +214,24 @@ function initializeSlider(dataSlider) {
   const sliderContainer = document.querySelector('.slider-container')
 
   /** 
-    STYLES CLASS
+    STYLES: VARIABLES DESING 
   **/
 
   sliderContainer.classList.add(SITE)
+
+  const variablesStyles = {
+    '--gap-mobile': DESING.mobile.gapSlider,
+    '--width-img-mobile': DESING.mobile.widthItem,
+    '--height-img-mobile': DESING.mobile.heightItem,
+    '--gap-desktop': DESING.desktop.gapSlider,
+    '--width-img-desktop': DESING.desktop.widthItem,
+    '--height-img-desktop': DESING.desktop.heightItem
+  }
+
+  const styles = sliderContainer.style
+  for (const [property, value] of Object.entries(variablesStyles)) {
+    styles.setProperty(property, value)
+  }
 
   /** 
     CREATE ROWS FORMAT AND SLIDERS IN CONTAINER
@@ -252,18 +278,3 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
   }, 50)
 })
-
-// sliderParams = '{"items": 20, "itemsRow": 2, "speedItemsRow": [0.5, 1]}',
-
-// const scriptSlider = (
-//   sliderUrl: string,
-//   site: string,
-//   sliderParams: string
-// ): string =>
-//   // eslint-disable-next-line no-template-curly-in-string
-//   `"use strict";const SLIDER_URL="<SLIDER_URL>",SITE="<SITE>",SLIDER_PARAMS_S='<SLIDER_PARAMS_S>';const SLIDER_PARAMS=JSON.parse(SLIDER_PARAMS_S);const {items:ITEMS,itemsRow:ITEMS_ROW,speedItemsRow:SPEED_ITEMS_ROW}=SLIDER_PARAMS;document.addEventListener('DOMContentLoaded',async()=>{setTimeout(async()=>{try{const r=await fetch(SLIDER_URL),d=await r.json(),s=d?.slider||[];if(s?.length>=ITEMS){const e=t=>\`<a class="slide" href="\${t.link}" target="_blank"><picture><source media="(min-width:768px)" srcset="\${t.image}"><img src="\${t.imageMobile}" alt="\${t?.title}"/></picture><div class="slide-data \${SITE||''}"><h2>\${t?.title}</h2><p>\${t?.description}</p></div></a>\`,a=t=>\`<div class="slider">\${t.map(e).join('')}</div>\`,o=document.querySelector('.slider-wrapper'),c=Math.floor(s.length/ITEMS_ROW);o&&(o.innerHTML=Array.from({length:ITEMS_ROW},(_,t)=>{const o=s.slice(t*c,(t+1)*c);return o.concat(o)}).map(a).join(''),document.querySelectorAll('.slider').forEach((t,a)=>{!function(t,a,o){let c=0;const n=()=>{const e=t.firstElementChild.offsetWidth,r=e*o;c-=a,t.style.transform=\`translateX(\${c}px)\`,Math.abs(c)>=r&&(c=0),f===!1&&requestAnimationFrame(()=>n())};let f=!1;t.addEventListener('mouseover',()=>{f=!0}),t.addEventListener('mouseleave',()=>{f=!1,requestAnimationFrame(()=>n())}),requestAnimationFrame(()=>n())}(t,SPEED_ITEMS_ROW[a],c)}))}}catch(t){console.error(t)}},50)});function animateSlider(t,a,o){let c=0;const n=()=>{const e=t.firstElementChild.offsetWidth,r=e*o;c-=a,t.style.transform=\`translateX(\${c}px)\`,Math.abs(c)>=r&&(c=0),f===!1&&requestAnimationFrame(()=>n())};let f=!1;t.addEventListener('mouseover',()=>{f=!0}),t.addEventListener('mouseleave',()=>{f=!1,requestAnimationFrame(()=>n())}),requestAnimationFrame(()=>n())}`
-//     .replace(/<SLIDER_URL>/g, sliderUrl)
-//     .replace(/<SITE>/g, site)
-//     .replace(/<SLIDER_PARAMS_S>/g, JSON.stringify(JSON.parse(sliderParams)))
-
-// export default scriptSlider
